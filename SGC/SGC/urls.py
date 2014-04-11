d293278a7ae79b45a1a11cfeca627e0991664eee
nobu <nobu@nobu-Satellite-L755D>
2014-04-11 01:44:19 -0400

    Actualización del directorio templates.

--- SGC/SGC/urls.py
+++ SGC/SGC/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'SGC.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('home.urls')),
     url(r'^', include('adm.urls')),
-    url(r'^admin/', include(admin.site.urls)),
 )
