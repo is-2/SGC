94c436b888d3ba9b7ce70ddbba21a7d7af10420e
nobu <nobu@nobu-Satellite-L755D>
2014-04-11 14:21:02 -0400

    Creación de formulario para crear y modificar usuarios.

--- SGC/adm/admin.py
+++ SGC/adm/admin.py
@@ -1,6 +1,1 @@
 from django.contrib import admin
-from adm.models import cliente,producto,user
-
-admin.site.register(cliente)
-admin.site.register(producto)
-admin.site.register(user)
--- SGC/adm/forms.py
+++ SGC/adm/forms.py
@@ -1,22 +1,53 @@
 from django import forms
-
-# TEST FORMS
-
-class addProductForm(forms.Form):
-    name        = forms.CharField(widget=forms.TextInput())
-    description = forms.CharField(widget=forms.TextInput())
-    def clean(self):
-        return self.cleaned_data
-
-### REAL FORMS
+from django.contrib.auth.models import User
 
 class addUserForm(forms.Form):
-    username    = forms.CharField(widget=forms.TextInput())
-    password    = forms.CharField(widget=forms.TextInput())
-    # status missing
-    email       = forms.CharField(widget=forms.TextInput())
-    phonenum    = forms.CharField(widget=forms.TextInput())
-    direction   = forms.CharField(widget=forms.TextInput())
-    observation = forms.CharField(widget=forms.Textarea())
-    def clean(self):
-        return self.cleaned_data
+    username    = forms.CharField(label="username", widget=forms.TextInput(), required=True)
+    password    = forms.CharField(label="password", widget=forms.PasswordInput(render_value=False), required=True)
+    firstName   = forms.CharField(label="firstName", widget=forms.TextInput(), required=True)
+    lastName    = forms.CharField(label="lastName", widget=forms.TextInput(), required=True)
+    email       = forms.EmailField(label="email", widget=forms.TextInput(), required=True)
+    phonenum    = forms.CharField(label="phonenum", widget=forms.TextInput(), required=False)
+    direction   = forms.CharField(label="direction", widget=forms.TextInput(), required=False)
+    observation = forms.CharField(label="observation", widget=forms.TextInput(), required=False)
+
+    def clean_username(self):
+        username = self.cleanead_data['username']
+        try:
+            user = User.objects.get(username=username)
+            if user.username==username:
+                return username
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError('El nombre de usuario ya existe.')
+    
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        try:
+            user = User.objects.get(email=email)
+            if user.email == email:
+                return email
+        except User.DoesNotExist:
+            return email
+        raise forms.ValidationError('El e-mail ya existe.')
+    
+class ModUserForm(forms.Form):
+  
+    firstName   = forms.CharField(label="firstName", widget=forms.TextInput(), required=True)
+    lastName    = forms.CharField(label="lastName", widget=forms.TextInput(), required=True)
+    email       = forms.EmailField(label="email", widget=forms.TextInput(), required=True)
+    phonenum    = forms.CharField(label="phonenum", widget=forms.TextInput(), required=False)
+    direction   = forms.CharField(label="direction", widget=forms.TextInput(), required=False)
+    observation = forms.CharField(label="observation", widget=forms.TextInput(), required=False)
+    
+               
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        try:
+            user = User.objects.get(email=email)
+            if user.email == email:
+                return email
+        except User.DoesNotExist:
+            return email
+        raise forms.ValidationError('El e-mail ya existe.')
+    
