8c79401166c96040e91157d7947f5131e7dd4f0b
Akira Shimosoeda <akirashimosoeda@gmail.com>
2014-05-02 01:10:59 -0400

    Optimización de codigo. Mejora en la interfaz de asignaciones. Mejor jerarquización de los directorios de html y url.

--- SGC/adm/models.py
+++ SGC/adm/models.py
@@ -1,47 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-# PERMISSION
-class Permission(models.Model):
-    """
-    Clase que especifica el permiso mediante un codigo.
-    """
-    name     = models.CharField(max_length=30, blank=False)
-    
-    def __unicode__(self):
-        return self.name
-
-# ROLE
-class Role(models.Model):
-    """
-    Clase que especifica el rol. Entre sus atributos se encuentran los permisos.
-    """
-    name        = models.CharField(max_length=30, blank=False)
-    description = models.TextField(blank=True)
-    permissions = models.ManyToManyField(Permission, blank=False)
-    
-    def __unicode__(self):
-        return self.name
-
-# PROJECT
+# Create your models here.
 class Project(models.Model):
-    """
+    name = models.CharField("project's name", max_length=100)
+    description = models.CharField("description", max_length=100)
     
-    """
-    name = models.CharField(max_length=30, blank=False)
-    description = models.CharField(max_length=100, blank=False)
-    roles = models.ManyToManyField(Role, null=True, blank=True)
-    committee = models.ManyToManyField(User, null=True, blank=True)
-    #phases = models.ManyToManyField(Phase, null=True, blank=True)
+    #leader = models.ForeignKey(User, "the related leader")
+    #committees = models.ManyToManyField(User, through='ProjectMembership')
+    #groups = models.ManyToManyField(Group, "list of groups")
     
-    def __unicode__(self):
-        return self.name
-    
-# USER
-User.add_to_class('firstName', models.CharField(max_length=100, blank=True))
-User.add_to_class('lastName', models.CharField(max_length=100, blank=True))
-User.add_to_class('status', models.BooleanField(default=True))
-User.add_to_class('phonenum', models.CharField(max_length=100, blank=True))
-User.add_to_class('address', models.CharField(max_length=100, blank=True))
-User.add_to_class('observation', models.CharField(max_length=100, blank=True))
-User.add_to_class('roles', models.ManyToManyField(Role, null=True, blank=True))
+    class Meta:
+        permissions = (("puede_crear_proyecto","Puede crear proyecto"),
+                       ("puede_modificar_proyecto","Puede modificar proyecto"),
+                       ("puede_eliminar_proyecto","Puede eliminar proyecto"),)
+        
