791d581c75bfd5892ef049fe73c1d4ffaca78f52
Akira Shimosoeda <akirashimosoeda@gmail.com>
2014-06-06 17:46:03 -0400

    Mejorando la interfaz del sistema.

--- SGC/home/forms.py
+++ SGC/home/forms.py
@@ -9,7 +9,7 @@ class SignUpForm(forms.Form):
     username    = forms.CharField(label="username", widget=forms.TextInput, required=True)
     password    = forms.CharField(label="password", widget=forms.PasswordInput(render_value=False), required=True)
     email       = forms.EmailField(label="email", widget=forms.TextInput, required=True)
-    telephone   = forms.CharField(label="telephone", widget=forms.TextInput, required=True)
+    telephone   = forms.CharField(label="telefono", widget=forms.TextInput, required=True)
     
     def clean_username(self):
         username = self.cleaned_data['username']
@@ -19,7 +19,7 @@ class SignUpForm(forms.Form):
                 return username
         except User.DoesNotExist:
             return username
-        raise forms.ValidationError('Username already exists.')
+        raise forms.ValidationError('Username ya existe.')
     
     def clean_email(self):
         email = self.cleaned_data['email']
@@ -29,7 +29,7 @@ class SignUpForm(forms.Form):
                 return email
         except User.DoesNotExist:
             return email
-        raise forms.ValidationError('Email already exists.')
+        raise forms.ValidationError('Email ya existe.')
     
 
     
